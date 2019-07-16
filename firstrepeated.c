b383895d3880bf53dde929bb453e40548b11ea9d
MadhuNagaKumar <51699295+MadhuNagaKumar@users.noreply.github.com>
2019-07-16 13:27:31 +0530

    Update firstrepeated.c

--- firstrepeated.c
+++ firstrepeated.c
@@ -2,7 +2,7 @@
 #include<string.h>
  
 int main(){
-    int a,b[10],i,d=0,j,t[10],count=0;
+    int a,b[10],i,d=0,j,t[10],count=0,u=0;
     scanf("%d",&a);
     for(i=0;i<a;i++){
         scanf("%d",&b[i]);
@@ -11,29 +11,40 @@ int main(){
         for(j=0;j<a;j++){
             if(b[i]==b[j]){
                 count++;
+                u--;
                }
                if(count>1){
                    t[i]=j;
                    break;
                }
             }
+            if(b[i]!=b[j]){
+                u++;
+            }
         count=0;
     }
-    for(i=0;i<a;i++){
-        for(j=i+1;j<a;j++){
-            if(t[i]>=t[j]){
+    if(u<=0)
+    {
+        for(i=0;i<a;i++){
+            for(j=i+1;j<a;j++){
+                if(t[i]>=t[j]){
+                    d=t[i];
+                    t[i]=t[j];
+                    t[j]=d;
+                }
+            }
+        }
+        for(i=0;i<a;i++){
+            if(t[i]!=0){
                 d=t[i];
-                t[i]=t[j];
-                t[j]=d;
+                printf("%d",b[d]);
+                break;
             }
         }
     }
-    for(i=0;i<a;i++){
-        if(t[i]!=0){
-            d=t[i];
-            printf("%d",b[d]);
-            break;
-        }
+    else
+    {
+        printf("unique");
     }
     return 0;
 }
